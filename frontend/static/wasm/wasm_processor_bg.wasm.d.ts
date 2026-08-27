56d7b9dbbe18562d780a4953778c2f2bfcae3e4b
ballescript <ballescript@gmail.com>
2026-08-27 14:19:31 +0200

    Force add missing wasm files

---
+++ frontend/static/wasm/wasm_processor_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_pitchdetector_free: (a: number, b: number) => void;
+export const pitchdetector_detect_pitch: (a: number, b: number, c: number) => number;
+export const pitchdetector_new: (a: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_start: () => void;
