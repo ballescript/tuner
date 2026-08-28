40aec8c4481e632ad11458e764c4ede1f1af1493
ballescript <ballescript@gmail.com>
2026-08-28 14:48:08 +0200

    svelte config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,4 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -10,20 +9,7 @@ export default defineConfig({
             compilerOptions: {
                 runes: ({ filename }) =>
                     filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-            },
-            
-            // Note how paths and adapter are directly here, NOT wrapped in a 'kit' object
-            paths: {
-                base: '/tuner'
-            },
-            
-            adapter: adapter({
-                pages: 'build',
-                assets: 'build',
-                fallback: 'index.html',
-                precompress: false,
-                strict: true
-            })
+            }
         })
     ]
 });
