10af9c9628f370b673864fe09b53884f04a722fb
ballescript <ballescript@gmail.com>
2026-08-29 14:48:18 +0200

    errorssss

--- frontend/static/audio-processor.js
+++ frontend/static/audio-processor.js
@@ -4,39 +4,50 @@ class PitchDetectorProcessor extends AudioWorkletProcessor {
     constructor() {
         super();
         this.detector = null;
-        this.frameCount = 0;
-        
+
+        // TRACER 1: Did the Worklet even boot?
+        this.port.postMessage({ type: 'error', message: 'TRACER: Worklet Constructor Ran' });
+
         this.port.onmessage = async (event) => {
-    if (event.data.type === 'init-wasm') {
-        try {
-            await init(event.data.wasmModule);
-            this.detector = PitchDetector.new(event.data.sampleRate);
-            this.port.postMessage({ type: 'ready' });
-        } catch (err) {
-            // This catches the silent mobile crash and sends it to our debug log!
-            this.port.postMessage({ 
-                type: 'error', 
-                message: "INIT FAIL: " + (err.message || String(err)) 
-            });
-        }
-    }
-};
+            // TRACER 2: Did it receive the WASM module?
+            this.port.postMessage({ type: 'error', message: 'TRACER: Received message in Worklet' });
+
+            if (event.data.type === 'init-wasm') {
+                try {
+                    // TRACER 3: About to run Rust
+                    this.port.postMessage({ type: 'error', message: 'TRACER: Starting init()...' });
+                    
+                    await init(event.data.wasmModule);
+                    
+                    // TRACER 4: Rust booted successfully
+                    this.port.postMessage({ type: 'error', message: 'TRACER: init() finished. Creating detector.' });
+                    
+                    this.detector = PitchDetector.new(event.data.sampleRate);
+                    
+                    // TRACER 5: Detector created
+                    this.port.postMessage({ type: 'error', message: 'TRACER: Detector created!' });
+                    
+                    this.port.postMessage({ type: 'ready' });
+                } catch (err) {
+                    this.port.postMessage({ 
+                        type: 'error', 
+                        message: "WASM INIT FAIL: " + (err.message || String(err)) 
+                    });
+                }
+            }
+        };
     }
 
     process(inputs) {
-        if (!this.detector) return true; 
-
-        const input = inputs[0];
+        if (!this.detector) return true;
         
+        const input = inputs[0];
         if (input && input.length > 0 && input[0].length > 0) {
-            const hz = this.detector.detect_pitch(input[0]);
-            
-            this.frameCount++;
-            if (this.frameCount % 15 === 0 && hz > 0) {
-                this.port.postMessage({ type: 'pitch', hz: hz });
+            const pitch = this.detector.detect_pitch(input[0]);
+            if (pitch > 0) {
+                this.port.postMessage({ type: 'pitch', hz: pitch });
             }
         }
-
         return true;
     }
 }
