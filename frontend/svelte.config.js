c2859d667e7b8d4c7c05fecc664cf7b69122a60c
ballescript <ballescript@gmail.com>
2026-09-01 14:17:52 +0200

    svelte config

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     
     compilerOptions: {
-        runes: ({ filename }: { filename: string }) =>
+        /** @param {{ filename: string }} param0 */
+        runes: ({ filename }) =>
             filename.split(/[/\\]/).includes('node_modules') ? undefined : true
     },
 
